de1d94fae78cbf2465e7d3fdad66124f5736fe4a
Malvika0108 <malvika.koushik@tamu.edu>
2025-10-22 19:39:05 -0500

    ITEM CRUD Operations and Search Functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,13 @@ class HomeController < ApplicationController
 
   def index
     @user = current_user
+
+    @items = Item.where(available: true) 
+    
+    if params[:query].present?
+      @items = @items.where("title ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    end
+    
+    @items = @items.order(created_at: :desc)
   end
 end
